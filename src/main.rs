b79ded8ef8842b1ba122d3608f3030a07147ffb6
abdul <abdul.emad0@gmail.com>
2025-01-09 23:09:54 +0000

    implement blockchain struct

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(
-        0,
-        0,
-        vec![0; 32],
-        0,
-        "First Block".to_owned(),
-        0x00000fffffffffffffffffffffffffff,
-    );
-
-    println!("{:?}", &block);
-
-    let h = block.hash();
-
-    println!("{:?}", &h);
-
-    block.hash = h;
-
-    println!("{:?}", &block);
+    let difficulty = 0x0000ffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "First Block".to_owned(), difficulty);
 
     block.mine();
 
-    println!("{:?}", &block);
+    println!("mined first block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, 0, last_hash, 0, "New block".to_owned(), difficulty);
+        block.mine();
+        println!("mined new block {:?}", block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+    }
 }
