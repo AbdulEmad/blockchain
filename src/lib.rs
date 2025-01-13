6aa8921007dca6378506619a07a6c31a781f2521
abdul <abdul.emad0@gmail.com>
2025-01-13 22:43:55 +0000

    implement transaction

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 type BlockHash = Vec<u8>;
+type Address = String;
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -25,3 +26,6 @@ pub use crate::hashable::Hashable;
 
 mod blockchain;
 pub use crate::blockchain::Blockchain;
+
+mod transaction;
+pub use crate::transaction::Transaction;
