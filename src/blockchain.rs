2e884dd0afae86d457eea880f8521b0ac4c50f46
abdul <abdul.emad0@gmail.com>
2025-01-15 23:15:32 +0000

    transactions

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -7,7 +7,7 @@ pub enum BlockValidationErr {
     InvalidFirstBlock,
     InvalidInput,
     InsufficientInputValue,
-    InvalidCoinbaseTransaction
+    InvalidCoinbaseTransaction,
 }
 
 pub struct Blockchain {
@@ -15,12 +15,12 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn update_with_block (&self, block: Block) -> Result<(), BlockValidationError>{
+    pub fn update_with_block(&self, block: Block) -> Result<(), BlockValidationError> {
         let i = self.blocks.len();
         if block.index != i as u32 {
             return Err(BlockValidationError::MistmatchedIndex);
         } else if !block::check_difficulty(&block.hash(), block.difficulty) {
-            return Err(BlockValidationErrror::InvalidHash);    
+            return Err(BlockValidationErrror::InvalidHash);
         } else if i != 0 {
             let prev_block = &self.blocks[i - 1];
             if block.timestamp <= prev_block.timestamp {
@@ -33,15 +33,46 @@ impl Blockchain {
                 return Err(BlockValidationError::InvalidFirstBlock);
             }
         }
-    }
-    if let Some((coinbase, transactions)) = block.transactions.split_first() {
-        if !coinbase.is_coinbase(){
-            return Err(BlockValidationErr:InvalidCoinbaseTransaction);
-        }
-        let mut block_spent = HashSet::new();
-    }
+        if let Some((coinbase, transactions)) = block.transactions.split_first() {
+            if !coinbase.is_coinbase() {
+                return Err(BlockValidationErr::InvalidCoinbaseTransaction);
+            }
+            let mut block_spent = HashSet::new();
 
-    let mut block_spent: HashSet<Hash> = HashSet::new();
-    Ok(()) 
+            let mut block_spent: HashSet<Hash> = HashSet::new();
+            let mut total_fee = 0;
+
+            for transaction in transactions {
+                let input_hashes = transaction.input_hashes();
+                if !(&input_hashes - &self.unspent_outputs).is_empty()
+                    || &(&input_hashes & &block_spent).is_empty()
+                {
+                    return Err(BlockChainValidationErr::invalidInput);
+                }
+
+                let input_value = transaction.input_value();
+                let output_value = transaction.output_value();
+
+                if output_value > input_value {
+                    return Err(BlockValidationError::insufficientInputValue);
+                }
+
+                let fee = input_value - output_value;
+                total_fee += fee;
+                block_spent.extend(input_hashes);
+                block_created.extend(transaction.output_hashes);
+            }
+
+            if coinbase.output_value() < total_fee {
+                return Err(BlockValidationErr::InvalidCoinbaseTransaction);
+            } else {
+                block_created.extend(coinbase.output_hashes());
+            }
+
+            self.unspent_outputs
+                .retain(|output| !block_spent.contains(output));
+            self.unspent_outputs.extend(block_created);
+        }
+        Ok(())
     }
 }
